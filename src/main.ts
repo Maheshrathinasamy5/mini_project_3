204b9788a53afde8dfa6b2afe70ff80007025bfc
Maheshrathinasamy5 <maheshrathinasamy5@gmail.com>
2025-03-15 21:42:14 +0530

    E-Commerce

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideAnimations(),
+    provideRouter([]),
+    importProvidersFrom(SocketIoModule.forRoot(config))
+  ]
+}).catch(err => console.error(err));
